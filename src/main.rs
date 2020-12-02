4786f436939a608e5163e931b3540c0ccef40cd2
deckvrd <deckvrd@gmail.com>
2020-12-02 17:44:41 -0500

    feat: rng
    Generate a random number using the rand crate and print it to the
    console.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess a number!");
+
+    let secret_num = rand::thread_rng().gen_range(1,101);
+    println!("The secret number is: {}", secret_num);
+
     println!("Please input your guess.");
     let mut guess = String::new();
 
