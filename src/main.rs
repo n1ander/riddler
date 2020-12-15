3dbc20b8e5ae414908e92d066214aac2ef51e9f9
deckvrd <deckvrd@gmail.com>
2020-12-15 11:00:44 -0500

    feat: multiple guesses
    Allow the user to guess more than once. The game will stop once the user
    guesses the correct number.

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,28 @@ fn main() {
     //generate a random number
     let secret_num = rand::thread_rng().gen_range(1,101);
     println!("The secret number is: {}", secret_num);
-    println!("Please input your guess.");
-    //create a mutable string variable to hold the guess
-    let mut guess = String::new();
-    //store user input
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line!");
-    //convert guess from string to u32
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    //print the guess to console
-    println!("You guessed: {}", guess);
 
-    //compare guess with generated number
-    match guess.cmp(&secret_num){
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("Congrats! You guessed the right number.")
+    loop{
+        println!("Please input your guess.");
+        //create a mutable string variable to hold the guess
+        let mut guess = String::new();
+        //store user input
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
+        //convert guess from string to u32
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        //print the guess to console
+        println!("You guessed: {}", guess);
+
+        //compare guess with generated number
+        match guess.cmp(&secret_num){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("Congrats! You guessed the right number.");
+                break;
+            }
+        }
     }
 }
